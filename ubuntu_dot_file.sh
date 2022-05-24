aa6de8b2866fa8d50cc441450b430d150d9de18c
mbranbilla <mbranbilla@outlook.com>
2022-05-24 16:48:10 -0300

    Add Python and R installation and packages

--- ubuntu_dot_file.sh
+++ ubuntu_dot_file.sh
@@ -37,6 +37,28 @@ apt update -y && apt install google-chrome-stable -y
 # Install Slack
 flatpak install flathub com.slack.Slack -y
 
+# Install Python and packages
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update -y
+apt install python3 -y
+
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install --upgrade pip numpy pandas scikit-learn matplotlib seaborn imbalanced-learn
+python3 -m pip install --upgrade pip jupyter jupyterlab ipython ipykernel
+
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
+
+# R Software
+apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 -y
+add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/' -y
+
+apt install r-base -y
+
+# R Studio
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2022.02.1-461-amd64.deb
+dpkg -i rstudio-2022.02.1-461-amd64.deb
 
 
 # Some utils stufs
